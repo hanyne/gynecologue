e057817bc7070d0f0bfd28a21a5acbb243645df5
hanine Benali <100847640+hanyne@users.noreply.github.com>
2023-04-17 18:01:58 +0200

    modifier la liste des patients
    make it adaptable with the carnet

--- frontend/src/app/components/docteur/list-patientes/list-patientes.component.ts
+++ frontend/src/app/components/docteur/list-patientes/list-patientes.component.ts
@@ -1,53 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild} from '@angular/core';
-import { PatienteService } from 'src/app/service/patiente.service';
-import { Patiente } from 'src/app/model/patiente';
+import { carnetService } from 'src/app/service/carnet.service';
+import { Carnet} from 'src/app/model/carnet';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import {  FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-list-patientes',
   templateUrl: './list-patientes.component.html',
   styleUrls: ['./list-patientes.component.css']
 })
 export class ListPatientesComponent implements OnInit {
-  textsearch:any;
-  patientes: Patiente[]=[];
-  dataSource = new MatTableDataSource<Patiente>(this.patientes);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  carnets!:Carnet;
+  Carnet:any = [];
+  constructor(public fb: FormBuilder,private CarnetService: carnetService) { 
+    this.readCarnet();
   }
-  
-  constructor( private patienteService:PatienteService) { }
+  ngOnInit() {}
+  readCarnet(){
+    this.CarnetService.getCarnets().subscribe((data) => {
+     this.Carnet = data;
 
-  ngOnInit() {
-    this.getAll();
+    })    
   }
-
-  getAll(){
-    this.patienteService.getP().subscribe((data) =>
-    {
-      this.patientes = data
-      console.log(data);
-    } )
-
-  }
-  async logOut() {
-    if (confirm("Do you want to log out?")) {
-      await this.patienteService.logoutUser()
+  onDelete(carnet: Carnet) {
+    if (carnet && carnet._id && confirm(`Souhaitez-vous confirmer la suppression de carnet de"${carnet.nom}"?`)) {
+      this.CarnetService.deleteCarnet(carnet._id).subscribe(
+        () => {
+          const index = this.carnets.findIndex((a: { _id: string | undefined; }) => a._id === carnet._id);
+          this.carnets.splice(index, 1);
+        },
+        (err) => console.error(err)
+      );
     }
   }
   
-  deletepatiente(patienteClicked: Patiente) {
-    if(window.confirm('Do you want to go ahead?')) {
-
-    this.patienteService.deleteP(patienteClicked._id)
-      .subscribe(
-        () => {
-          this.patientes = this.patientes.filter(tL => tL._id != patienteClicked._id);
-        }
-      );}
-  }
 }
